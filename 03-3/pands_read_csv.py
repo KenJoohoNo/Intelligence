a5d56d12b99a5db1a2c6fbea4a0e7b67aa744de1
Pyeonno <pyeonno13634@gmail.com>
2025-04-17 12:04:51 +0900

    Lasso

--- 03-3/pands_read_csv.py
+++ 03-3/pands_read_csv.py
@@ -4,7 +4,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import Ridge
+# from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
 import matplotlib.pyplot as plt
 
 perch_full = pd.read_csv('https://bit.ly/perch_csv_data')
@@ -62,27 +63,58 @@ ss.fit(train_poly)
 train_scaled = ss.transform(train_poly)
 test_scaled = ss.transform(test_poly)
 
-ridge = Ridge()
-ridge.fit(train_scaled, train_target)
-print(ridge.score(train_scaled, train_target))
-print(ridge.score(test_scaled, test_target))
+# ridge = Ridge()
+# ridge.fit(train_scaled, train_target)
+# print(ridge.score(train_scaled, train_target))
+# print(ridge.score(test_scaled, test_target))
+
+# train_score = []
+# test_score = []
+
+# alpha_list = [0.001, 0.01, 0.1, 1, 10, 100]
+# for alpha in alpha_list:
+#     # 릿지 모델 생성
+#     ridge = Ridge(alpha=alpha)
+#     # 릿지 모델 훈련
+#     ridge.fit(train_scaled, train_target)
+#     # 훈련 점수와 테스트 점수를 지정
+#     train_score.append(ridge.score(train_scaled, train_target))
+#     test_score.append(ridge.score(test_scaled, test_target))
+
+# plt.plot(alpha_list, train_score)
+# plt.plot(alpha_list, test_score)
+# plt.xscale('log')
+# plt.xlabel('alpha')
+# plt.ylabel('R^2')
+# plt.show()
+
+lasso = Lasso()
+lasso.fit(train_scaled, train_target)
+# print(lasso.score(train_scaled, train_target))
+# print(lasso.score(test_scaled, test_target))
 
 train_score = []
 test_score = []
-
 alpha_list = [0.001, 0.01, 0.1, 1, 10, 100]
 for alpha in alpha_list:
-    # 릿지 모델 생성
-    ridge = Ridge(alpha=alpha)
-    # 릿지 모델 훈련
-    ridge.fit(train_scaled, train_target)
-    # 훈련 점수와 테스트 점수를 지정
-    train_score.append(ridge.score(train_scaled, train_target))
-    test_score.append(ridge.score(test_scaled, test_target))
+	# 라쏘 모델 생성
+    lasso = Lasso(alpha=alpha, max_iter=10000, tol=1e-2)
+    # 라쏘 모델 훈련
+    lasso.fit(train_scaled, train_target)
+    # 훈련 점수와 테스트 점수를 저장
+    train_score.append(lasso.score(train_scaled, train_target))
+    test_score.append(lasso.score(test_scaled, test_target))
 
 plt.plot(alpha_list, train_score)
 plt.plot(alpha_list, test_score)
 plt.xscale('log')
 plt.xlabel('alpha')
 plt.ylabel('R^2')
-plt.show()
+# plt.show()
+
+lasso = Lasso(alpha=10)
+lasso.fit(train_scaled, train_target)
+print(lasso.score(train_scaled, train_target))
+print(lasso.score(test_scaled, test_target))
+
+print(np.sum(lasso.coef_ == 0))
